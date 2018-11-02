0c0c5e7b7789e8d843c537cec6db978f22655320
Jacob Quant <jacobq@gmail.com>
2018-11-02 15:47:04 -0500

    fix: generate type definition file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
+const { resolve } = require('path');
+
 module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    'src/(.*)': resolve('src/$1'),
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
