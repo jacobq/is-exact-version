cd002fc143227024c0bf3345863f95246148a855
Jacob Quant <jacobq@gmail.com>
2022-03-08 10:14:37 -0600

    style: eslint --fix .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    extends: "airbnb-base",
-    parser: "@typescript-eslint/parser",
-    plugins: ["jest"],
-    root: true,
-    overrides: [{
-        files: ["tests/**/*"],
-        env: {
-            "jest/globals": true
-        },
-        rules: {
-        }
-    }]
+  extends: 'airbnb-base',
+  parser: '@typescript-eslint/parser',
+  plugins: ['jest'],
+  root: true,
+  overrides: [{
+    files: ['tests/**/*'],
+    env: {
+      'jest/globals': true,
+    },
+    rules: {
+    },
+  }],
 };
