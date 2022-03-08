9f0eb8a9ce775e357af6e6db7240d15bf4db3d15
Jacob Quant <jacobq@gmail.com>
2022-03-08 10:28:03 -0600

    feat: use ESM instead of CJS
    BREAKING CHANGE: ECMAScript modules require Node.js >= 12
    and also changes how projects load this one.
    https://nodejs.org/api/esm.html

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-const { resolve } = require('path');
+import { resolve } from 'path';
 
-module.exports = {
+export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   moduleNameMapper: {
